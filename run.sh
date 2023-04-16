c0c9c08192ed1ad0e9401d0df9127133d400f96e
hyj <2237853288@qq.com>
2023-04-16 23:41:40 +0800

    master

--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
-cd /usr/local/go_pro/twenty_game/srv/user_srv
+cd ./twenty_game/srv/user_srv
 go build
 ./user_srv &
 
-cd ../game_srv
-go build
-./game_srv &
-
-cd /usr/local/go_pro/twenty_game/web/user_web
+cd -
+cd ./twenty_game/web/user_web
 go build
 ./user_web &
 
-cd ../game_web
+cd -
+cd ./twenty_game/srv/game_srv
+go build
+./game_srv &
+
+cd -
+cd ./twenty_game/web/game_web
 go build
 ./game_web &
--- srv/game_srv/utils/ser.go
+++ srv/game_srv/utils/ser.go
@@ -34,7 +34,8 @@ func RegistAndHealthCheck(server *grpc.Server, port int) (*api.Client, string) {
 	reg.Address = global.ServerConfig.ConsulInfo.ServerHost //消费者访问服务地址
 	if !global.DEBUG {
 		reg.Check = &api.AgentServiceCheck{
-			GRPC: fmt.Sprintf("%s:%d", global.ServerConfig.Host, port),
+			GRPC:     fmt.Sprintf("%s:%d", global.ServerConfig.Host, port),
+			Interval: "3600",
 		}
 	}
 
