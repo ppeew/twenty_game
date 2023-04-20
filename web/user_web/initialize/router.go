1591df899f8561630cdda5f6c734c3be0190996a
hyj <2237853288@qq.com>
2023-04-20 11:27:51 +0800

    master

--- web/user_web/initialize/router.go
+++ web/user_web/initialize/router.go
@@ -2,13 +2,14 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"user_web/middlewares"
 	"user_web/router"
 )
 
 func InitRouters() *gin.Engine {
 	engine := gin.Default()
 	//中间件
-	//engine.Use()
+	engine.Use(middlewares.Cors())
 	versionGroup := engine.Group("/v1")
 	router.InitUserRouter(versionGroup)
 
