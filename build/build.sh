dd52a3d455909c55bbf1b31f1b47535c6700a4e2
hyj <2237853288@qq.com>
2023-08-06 00:45:31 +0800

    修改目录结构

--- build/build.sh
+++ build/build.sh
@@ -1,29 +1,28 @@
 
-cd ~/twenty_game/srv/user_srv
+cd ~/twenty_game/cmd/user_srv
 go build
 nohup ./user_srv > ../../us.log 2>&1 &
 
-cd ~/twenty_game/srv/game_srv
+cd ~/twenty_game/cmd/game_srv
 go build
 nohup ./game_srv > ../../gs.log 2>&1 &
 
-cd ~/twenty_game/web/game_web
+cd ~/twenty_game/cmd/game_web
 go build
 nohup ./game_web > ../../gw.log 2>&1 &
 
-cd ~/twenty_game/web/user_web
+cd ~/twenty_game/cmd/user_web
 go build
 nohup ./user_web > ../../uw.log 2>&1 &
 
-cd ~/twenty_game/web/file_web
+cd ~/twenty_game/cmd/file_web
 go build
 nohup ./file_web > ../../fw.log 2>&1 &
 
-cd ~/twenty_game/web/process_web
+cd ~/twenty_game/cmd/process_web
 go build
 nohup ./process_web > ../../pw.log 2>&1 &
 
-
-cd ~/twenty_game/web/store_web
+cd ~/twenty_game/cmd/store_web
 go build
 nohup ./store_web > ../../sw.log 2>&1 &
