0590bd27d2c56336e361a92c118138a8d426af34
unitiny <2069234934@qq.com>
2023-08-08 21:27:00 +0800

    zeabur dockerfile

--- build/zeabur.sh
+++ build/zeabur.sh
@@ -1,37 +1,7 @@
-
-cd twenty_game/cmd/Game-user_srv
-go mod tidy
-go build -o user_srv main.go
-nohup ./user_srv > ../../us.log 2>&1 &
-
-cd twenty_game/cmd/Game-game_srv
-go mod tidy
-go build -o game_srv main.go
-nohup ./game_srv > ../../gs.log 2>&1 &
-
-cd twenty_game/cmd/Game-game_web
-go mod tidy
-go build -o game_web main.go
-nohup ./game_web > ../../gw.log 2>&1 &
-
-cd twenty_game/cmd/Game-user_web
-go mod tidy
-go build -o user_web main.go
-nohup ./user_web > ../../uw.log 2>&1 &
-
-cd twenty_game/cmd/Game-file_web
-go mod tidy
-go build -o file_web main.go
-nohup ./file_web > ../../fw.log 2>&1 &
-
-cd twenty_game/cmd/Game-process_web
-go mod tidy
-go build -o process_web main.go
-nohup ./process_web > ../../pw.log 2>&1 &
-
-cd twenty_game/cmd/Game-store_web
-go mod tidy
-go build -o store_web main.go
-nohup ./store_web > ../../sw.log 2>&1 &
-
-echo "build finish"
+nohup twenty_game/cmd/Game-user_srv/user_srv &
+nohup twenty_game/cmd/Game-game_srv/game_srv &
+nohup twenty_game/cmd/Game-process_web/process_web &
+nohup twenty_game/cmd/Game-user_web/user_web &
+nohup twenty_game/cmd/Game-game_web/game_web &
+nohup twenty_game/cmd/Game-store_web/store_web &
+nohup twenty_game/cmd/Game-file_web/file_web &
