0035c80dc1d1833f98af58b2b010e36e400c5258
unitiny <2069234934@qq.com>
2023-08-08 21:55:28 +0800

    zeabur dockerfile

--- build/zeabur.sh
+++ build/zeabur.sh
@@ -1,8 +1,14 @@
-nohup ./twenty_game/cmd/Game-user_srv/user_srv &
+nohup ./twenty_game/cmd/Game-user_srv/user_srv > ./twenty_game/us.log &
 nohup ./twenty_game/cmd/Game-game_srv/game_srv &
 nohup ./twenty_game/cmd/Game-process_web/process_web &
-nohup ./twenty_game/cmd/Game-user_web/user_web &
+nohup ./twenty_game/cmd/Game-user_web/user_web > ./twenty_game/uw.log &
 nohup ./twenty_game/cmd/Game-game_web/game_web &
 nohup ./twenty_game/cmd/Game-store_web/store_web &
 nohup ./twenty_game/cmd/Game-file_web/file_web &
+
+netstat -ntlup | grep srv
+netstat -ntlup | grep web
+
+cat ./twenty_game/us.log
+cat ./twenty_game/uw.log
 echo "finish"
