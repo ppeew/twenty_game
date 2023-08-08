cb9d75a1b919673bc2b27896f21bd5bce9b31f5f
unitiny <2069234934@qq.com>
2023-08-08 22:04:38 +0800

    zeabur dockerfile

--- build/zeabur.sh
+++ build/zeabur.sh
@@ -1,14 +1,29 @@
-nohup ./twenty_game/cmd/Game-user_srv/user_srv > ./twenty_game/us.log &
-nohup ./twenty_game/cmd/Game-game_srv/game_srv &
-nohup ./twenty_game/cmd/Game-process_web/process_web &
-nohup ./twenty_game/cmd/Game-user_web/user_web > ./twenty_game/uw.log &
-nohup ./twenty_game/cmd/Game-game_web/game_web &
-nohup ./twenty_game/cmd/Game-store_web/store_web &
-nohup ./twenty_game/cmd/Game-file_web/file_web &
 
-netstat -ntlup | grep srv
-netstat -ntlup | grep web
+cd ./twenty_game/cmd/Game-user_srv
+nohup ./user_srv > ../../us.log &
 
+cd ../Game-game_srv
+nohup ./game_srv &
+
+cd ../Game-process_web
+nohup ./process_web &
+
+cd ../Game-user_web
+nohup ./user_web > ../../uw.log &
+
+cd ../Game-game_web
+nohup ./game_web &
+
+cd ../Game-store_web
+nohup ./store_web &
+
+cd ../Game-file_web
+nohup ./file_web &
+
+ps -ef | grep srv
+ps -ef | grep web
+
+cd ../../
 cat ./twenty_game/us.log
 cat ./twenty_game/uw.log
 echo "finish"
