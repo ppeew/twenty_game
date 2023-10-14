4aca4ac4bfa889b39165fb966f68bd0c5cd3ad7f
unitiny <2069234934@qq.com>
2023-10-14 12:22:12 +0800

    initTask

--- build/jenkins-build.sh
+++ build/jenkins-build.sh
@@ -2,38 +2,39 @@ echo "jenkins-build start"
 
 cd ~/twenty_game/cmd/Game-user_srv
 go build -o user_srv main.go
-nohup ./user_srv &
+nohup ./user_srv > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-game_srv
 go build -o game_srv main.go
-nohup ./game_srv &
+nohup ./game_srv > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-game_web
 go build -o game_web main.go
-nohup ./game_web &
+nohup ./game_web > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-user_web
 go build -o user_web main.go
-nohup ./user_web &
+nohup ./user_web > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-file_web
 go build -o file_web main.go
-nohup ./file_web &
+nohup ./file_web > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-process_web
 go build -o process_web main.go
-nohup ./process_web &
+nohup ./process_web > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-store_web
 go build -o store_web main.go
-nohup ./store_web &
+nohup ./store_web > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-hall_web
 go build -o hall_web main.go
-nohup ./hall_web &
+nohup ./hall_web > /dev/null 2>&1 &
 
 cd ~/twenty_game/cmd/Game-admin_web
 go build -o admin_web main.go
-nohup ./admin_web &
+nohup ./admin_web > /dev/null 2>&1 &
 
+sleep 2s
 echo "jenkis-build finish"
